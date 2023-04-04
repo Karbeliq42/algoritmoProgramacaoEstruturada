71cf6f339648ddb4e1a5ba10e7f231711c34e141
Fabio Caixeta <fabio13caixeta@gmail.com>
2023-04-04 20:40:49 -0300

    listas2

--- lista1_02.c
+++ lista1_02.c
@@ -1,20 +1,22 @@
-#include <stdio.h>
-
-
-/*2. Faça um algoritmo que imprima o poema abaixo, com a mesma formatação:
+/*
 Caderno de poesias é um belo lugar.
 Tantas coisas lindas que eu gostaria de falar.
 Eu falo em forma de versos para todos poderem escutar.
 Agora você já sabe por que os poetas passam os dias escrevendo em seus
-cadernos de poesias*/
+cadernos de poesias. 
+*/
+
+#include<stdio.h>
 
 int main(){
 
+printf("Caderno de poesias é um belo lugar.\nTantas coisas lindas que eu gostaria de falar.\n
+Eu falo em forma de versos para todos poderem escutar.\nAgora você já sabe por que os poetas passam os dias escrevendo em seus
+cadernos de poesias.");
+
 printf("Caderno de poesias é um belo lugar.\n");
-  printf("Tantas coisas lindas que eu gostaria de falar.\n");
-  printf("Eu falo em forma de versos para todos poderem escutar.\n");
-  printf("Agora você já sabe por que os poetas passam os dias escrevendo em  seus cadernos de poesias.\n");
+printf("Tantas coisas lindas que eu gostaria de falar.\n");
+printf("Eu falo em forma de versos para todos poderem escutar.\n");
+printf("Agora você já sabe por que os poetas passam os dias escrevendo em seus\ncadernos de poesias.");
 
-
-    return 0;
 }
